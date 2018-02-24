ad12856a688a6a4ac0018b76eabf9141d58f1c26
jackie <jcqnly@gmail.com>
2018-02-23 18:34:34 -0800

    attempt to add click counter

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
 'use strict';
 
 //array of products
+Product.names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
 Product.allProducts = [];
+Product.viewed = []; //prevent duplicates
+Product.counter = 0;
+
+//DOM Access
+Product.container = document.getElementById('container');
+Product.pics = [
+  document.getElementById('left'),
+  document.getElementById('center'),
+  document.getElementById('right'),
+];
+Product.list = document.getElementById('productLists');
 
 //make an object of Bus Mall Products
 function Product(name, filepath) {
   this.name = name;
   this.filepath = filepath;
+  this.votes = 0;
+  this.views = 0;
   Product.allProducts.push(this);
 }
 
@@ -32,52 +47,44 @@ new Product('water-can', 'img/water-can.jpg');
 new Product('wine-glass', 'img/wine-glass.jpg');
 new Product('usb', 'img/usb.png');
 
-var productPic = document.getElementById('product');
-var productPic2 = document.getElementById('product2');
-var productPic3 = document.getElementById('product3');
-var counter = 0;
-// var clicksPerPic = [];
-
-//randomly display 1 of the pictures
-function randomProductPicker() {
-  var threeProducts = [];
-  //counter to make remove eventhandler
-  if(counter === 24) {
-    productPic.removeEventListener('click', randomProductPicker);
-    productPic2.removeEventListener('click', randomProductPicker);
-    productPic3.removeEventListener('click', randomProductPicker);
-  }
-  //check for dupes within the trio
-  var numbersMatch = true;
-  while(numbersMatch) {
-    for(var i = 0; i < 3; i++) {
-      var randomProduct = Math.floor(Math.random() * Product.allProducts.length);
-      threeProducts.push(randomProduct);
-      console.table(threeProducts);
-    }
-    // console.log(Product.allProducts[randomProduct]);
-    if(threeProducts[0] !== threeProducts[1] && threeProducts[1] !== threeProducts[2] && threeProducts[0] !== threeProducts[2]) {
-      //Assign the src, alt, and title attribute to the img element
-      productPic.src = Product.allProducts[threeProducts[0]].filepath;
-      productPic.alt = Product.allProducts[threeProducts[0]].name;
-      productPic.title = Product.allProducts[threeProducts[0]].name;
-      
-      productPic2.src = Product.allProducts[threeProducts[1]].filepath;
-      productPic2.alt = Product.allProducts[threeProducts[1]].name;
-      productPic2.title = Product.allProducts[threeProducts[1]].name;
-      
-      productPic3.src = Product.allProducts[threeProducts[2]].filepath;
-      productPic3.alt = Product.allProducts[threeProducts[2]].name;
-      productPic3.title = Product.allProducts[threeProducts[2]].name;
-      numbersMatch = false;
-    }
-    threeProducts = [];
-  }
-  counter++;
+function makeRandom() {
+  return Math.floor(Math.random() * Product.names.length);
 }
 
-randomProductPicker();
+function displayPics() {
+  while(Product.viewed.length < 6) {
+    var random = makeRandom();
+    while(!Product.viewed.includes(random)) {
+      Product.viewed.push(random);
+    }
+  }
+  for(var i = 0; i < 3; i++) {
+    var temp = Product.viewed.shift();
+    Product.pics[i].src = Product.allProducts[temp].filepath;
+    Product.pics[i].alt = Product.allProducts[temp].name;
+    Product.pics[i].title = Product.allProducts[temp].name;
+    Product.allProducts[temp].views += 1;
+  }
+}
 
-productPic.addEventListener('click', randomProductPicker);
-productPic2.addEventListener('click', randomProductPicker);
-productPic3.addEventListener('click', randomProductPicker);
+function handleClick(event) {
+  if(event.target === Product.container) {
+    return alert('click on an image');
+  }
+  Product.counter += 1;
+  console.log(Product.counter);
+
+  if(Product.counter > 24) {
+    Product.container.removeEventListener('click', handleClick);
+    Product.container.style.display = 'none';
+  }
+  for(var i = 0; i < Product.names.length; i++) {
+    if(event.target.alt === Product.allProducts[i].name) {
+      Product.allProducts[i].votes += 1;
+    }
+  }
+  displayPics();
+}
+
+displayPics();
+Product.container.addEventListener('click', handleClick);
